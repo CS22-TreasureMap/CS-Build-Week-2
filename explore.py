d176257643b62cc42f621948a9bb0a1decb736bc
PWalis <pwilky1016@gmail.com>
2019-11-20 14:14:32 -0700

    Cleaning again

--- explore.py
+++ explore.py
@@ -4,9 +4,9 @@ mappy = mapper()
 mappy.accumulate=False   #set pick up items to false
 mappy.import_text_map=True
 mappy.create_starting_map()
-mappy.wishing_well()
-mappy.hint_to_ld8()
-# mappy.go_to_room(55)
+# mappy.wishing_well()
+# mappy.hint_to_ld8()
+mappy.dash_to_room(461)
 
 # print(mappy.action(what='pray'))
 # print(mappy.action(what='status'))
