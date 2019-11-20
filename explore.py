6259af159bbb94f04a0da518c66d37d39aa7f7fc
PWalis <pwilky1016@gmail.com>
2019-11-20 13:51:34 -0700

    Cleaning my branch

--- explore.py
+++ explore.py
@@ -1,13 +1,17 @@
 from utils import *
 
 mappy = mapper()
-mappy.accumulate=True   #set pick up items to false
+mappy.accumulate=False   #set pick up items to false
 mappy.import_text_map=True
 mappy.create_starting_map()
-<<<<<<< HEAD
-mappy.explore_random(500)
-=======
+mappy.wishing_well()
+mappy.hint_to_ld8()
+# mappy.go_to_room(55)
+
+# print(mappy.action(what='pray'))
+# print(mappy.action(what='status'))
+# mappy.go_to_room(482)
+# mappy.get_proof()
 
 # mappy.explore_random(150)
-mappy.get_treasure()
->>>>>>> c46aac26a91ccffa80dc37130e3f88ee9df7bb73
+# mappy.get_treasure()
--- utils.py
+++ utils.py
@@ -34,121 +34,6 @@ class Player:
 
 
 class mapper:
-<<<<<<< HEAD
-  def __init__(self, auth=auth_key, save=True, load_map=True):
-    self.auth = auth  #the auth token
-    self.header = {'Authorization':f'Token {self.auth}'}   #the header for post and get
-    self.wait = 18  # the current sleep length - this is no longer required as wait always points to cooldown
-    self.info = {}   #the last status json from post or get
-    self.accumulate = False #whether player picks up items or not - it is very easy to get overencumbered
-    self.pray = False #can't pray without a name unfortunately
-    self.save_map_to_text = save  #save latest map to a text file
-    self.import_text_map = load_map #import map so far - setting to false starts from scratch
-    self.player = None
-    self.important_rooms = {}
-
-  def get_info(self, what='init', direction=None, backtrack=None):
-    """multi purpose move & init function - this is used
-    for the most common actions"""
-  
-    if what=='init':
-      response = requests.get(f'{my_url}{what}/',headers=self.header) 
-
-    elif what=='move':
-      response = requests.post(f'{my_url}move/',headers=self.header,json = {"direction":direction})
-
-    elif what=='backtrack':
-      response = requests.post(f'{my_url}move/',headers=self.header,json = {"direction":direction,"next_room_id": backtrack})   
-
-    if response.status_code==200:
-      self.info = json.loads(response.content)
-      if self.player is not None:
-        self.player.currentRoom = self.info['room_id']
-
-      if 'cooldown' in self.info.keys():  #there are a lot of TRAPS which require extra cooldown
-          time.sleep(self.info['cooldown'])
-
-      self.room_check()
-      return self.info
-    else:
-      print('cooldown triggered - waiting 20 seconds')
-      time.sleep(20)
-      self.get_info(what=what, direction=direction, backtrack=backtrack)
-
-  def action(self, what='take', treasure=None):
-    """another multi purpose request function
-    this one focuses on less common actions"""
-
-    if what in ['take','drop','sell','examine']:
-      response = requests.post(f'{my_url}{what}/', headers=self.header, json={"name":treasure})
-
-    if what in ['status','pray']:
-      response = requests.post(f'{my_url}{what}/', headers=self.header)
-
-    if what == 'confirm_sell':
-      response = requests.post(f'{my_url}{what}/', headers=self.header, json={"name":treasure, "confirm" : "yes"})
-
-    # Change Name +++++++
-    if what == 'change_name':
-      response = request.post(f'{my_url}{what}/', headers=self.headers, json={"name":new})
-    # Confirm Name +++++++
-    if what == 'confirm_name':
-      response = request.post(f'{my_url}{what}/', headers=self.headers, json={"confirm": "aye"})
-
-    if response.status_code==200:
-      self.info = json.loads(response.content)
-      if 'cooldown' in self.info.keys():
-          time.sleep(self.info['cooldown'])
-      return self.info
-    else:
-      print('error',what,treasure,response.status_code)
-
-  def room_check(self):
-    """checks for items in teh room or special rooms"""
-    #print('room check triggered.  info: ',self.info)
-    if self.info['items']!=[] and self.accumulate:
-      for item in self.info['items']:
-
-        self.info = self.action('take', item)
-        print(self.info)
-
-    if self.info['title'] == "Linh's Shrine" and self.pray:  #there may be other shrines
-      self.info = self.action('pray')
-
-    # Would this sell? ++++++
-    if self.info['title'] == "shop":
-      self.info = self.action('sell', item)
-      self.info = self.action('confirm_sell')
-
-    # Could this do the name change? +++++++
-    if self.info['title'] == "pirate ry": 
-      self.info = self.action('change_name', name)
-      self.info = self.action('confirm_name')
-    
-  def create_starting_map(self):
-    """"initiates your starting map which is stored under the vertices of a graph class"""
-    info_dict = self.get_info()
-    print(info_dict)   #this can be deactivated - just helpful at first
-    self.my_map = Graph()
-    self.player = Player("scooby_doo",info_dict['room_id'])
-    exits = info_dict['exits']
-    exit_dict = {}
-    for e in exits:
-      exit_dict[e] = '?'
-    if self.import_text_map:
-        print("load map triggered")
-        with open('map.txt','r') as file:
-            string_dict = json.loads(file.read())
-            for key in string_dict:
-                self.my_map.vertices[int(key)] = string_dict[key]
-        with open('rooms.txt', 'r') as file:
-            string_dict = json.loads(file.read())
-            for key in string_dict:
-                self.important_rooms[key] = string_dict[key]
-    else:
-        print("fresh map triggered")
-        self.my_map.vertices[self.player.currentRoom] = exit_dict
-=======
     def __init__(self, auth=auth_key, save=True, load_map=True):
         self.auth = auth  # the auth token
         # the header for post and get
@@ -223,7 +108,6 @@ class mapper:
         if what == 'change_name':
             response = requests.post(
                 f'{my_url}{what}/', headers=self.header, json={"name": name, "confirm": "aye"})
->>>>>>> c46aac26a91ccffa80dc37130e3f88ee9df7bb73
         
         if what == 'balance':
             response = requests.get(
@@ -260,58 +144,11 @@ class mapper:
         info_dict = self.get_info()
         # print(info_dict)  # this can be deactivated - just helpful at first
         self.my_map = Graph()
-        self.player = Player("MicahJones", info_dict['room_id'])
+        self.player = Player("Patrick", info_dict['room_id'])
         exits = info_dict['exits']
         exit_dict = {}
         for e in exits:
             exit_dict[e] = '?'
-<<<<<<< HEAD
-      self.my_map.vertices[new_room] = exit_dict
-    self.my_map.vertices[old_room][move] = new_room
-    reverse_move = reverse_dir[move]
-    self.my_map.vertices[new_room][reverse_move] = old_room
-    if self.save_map_to_text:
-        with open('map.txt','w') as file:
-            file.write(json.dumps(self.my_map.vertices))
-        
-    self.important_rooms.update({info['title']: info['room_id']})
-    if self.save_map_to_text:
-      with open('rooms.txt', 'w') as file:
-        file.write(json.dumps(self.important_rooms))
-
-  def count_unmapped(self):
-    """counts all the unmapped rooms"""
-    counter = 0
-    for val1 in self.my_map.vertices.values():
-      for val2 in val1.values():
-        if val2=='?':
-          counter += 1
-    return counter
-  
-  def get_dirs(self, traversal):
-    """gets the direction of travel given a room traversal list"""
-    point = traversal[0]
-    dir_list = []
-    for t in traversal[1:]:
-      for key in self.my_map.vertices[point]:
-        if self.my_map.vertices[point][key]==t:
-          dir_list.append(key)
-      point = t
-    return dir_list
-
-  def bfs_for_q(self):
-    """breadth first search for last ?"""
-    room = self.player.currentRoom
-    q = Queue()
-    q.enqueue([room])
-
-    while '?' not in self.my_map.vertices[room].values(): 
-      
-      joins = self.my_map.vertices[room]
-      for j in joins.values():
-        if j in q.queue[0]:
-          pass
-=======
         if self.import_text_map:
             print("load map triggered")
             with open('map.txt', 'r') as file:
@@ -323,7 +160,6 @@ class mapper:
                 string_dict = json.loads(file.read())
                 for key in string_dict:
                     self.important_rooms[key] = string_dict[key]
->>>>>>> c46aac26a91ccffa80dc37130e3f88ee9df7bb73
         else:
             print("fresh map triggered")
             self.my_map.vertices[self.player.currentRoom] = exit_dict
@@ -400,45 +236,6 @@ class mapper:
         """explores the map choosing random ? and backtracks using bfs
         counter is the number of times you want it to explore unknown rooms"""
         unmapped_number = self.count_unmapped()
-<<<<<<< HEAD
-        time.sleep(self.wait)
-      else:   
-        print('back track on') #leave this line in to show you when you are backtracking
-        backtrack = self.bfs_for_q()
-        backtrack_dirs = self.get_dirs(backtrack)
-        print('backtrack details',backtrack,backtrack_dirs) #this line shows details of backtrack
-        for i in range(len(backtrack_dirs)):
-          b_info = self.get_info('backtrack',backtrack_dirs[i],str(backtrack[i+1]))
-          self.player.currentRoom = b_info['room_id']
-      c+=1
-
-  def go_to_room(self, destination):
-    """depth first traversal to particular room in shortest route"""
-    self.accumulate = False
-    print('moving')
-    path = self.my_map.bfs(self.player.currentRoom, destination)
-    for m in path:
-      room = self.player.currentRoom
-      exits = self.my_map.vertices[room]
-      for direction in exits:
-        if self.my_map.vertices[room][direction] == m:
-          self.get_info(what='move', direction=direction)
-        else:
-          continue
-    self.accumulate = True
-
-  def pirate(self):
-    # Goes directly to pirate ry
-    # self.go_to_room(self.important_rooms['pirate ry'])
-    # time.sleep(self.wait)
-    pass
-
-  def wishing_well(self):
-    # Goes directly to pirate ry
-    # self.go_to_room(self.important_rooms['wishing well'])
-    # time.sleep(self.wait)
-    pass
-=======
         moves = []
         c = 0
         while unmapped_number > 0 and c <= counter:
@@ -584,7 +381,7 @@ class mapper:
             print("*******************************************************")
 
             #!------------------------This name is specific to each person, be sure to change this to yours.
-            if ret_data['name'] == 'player420' and ret_data['gold'] >= 1000:
+            if ret_data['name'] == 'player448' and ret_data['gold'] >= 1000:
                 # Go to name changer (pirate ry)
                 print('Time to Buy a Name')
                 # * Made this false here so that we don't somehow pick up a ton of treasure on the way, and
@@ -594,7 +391,7 @@ class mapper:
                 time.sleep(self.wait)
                 # Buy name
                 #! -------------------------- Change the name here to be what you want!!
-                self.action('change_name', name='SirSnuffaluffagus')
+                self.action('change_name', name=my_name)
                 time.sleep(self.wait)
 
                 #! This print isn't accurate. It doesn't update when you actually change your name.
@@ -718,55 +515,54 @@ class mapper:
 
     
 
->>>>>>> c46aac26a91ccffa80dc37130e3f88ee9df7bb73
 
-  def vendor(self):
-    # Goes directly to the shop
-    self.go_to_room(1)
-    time.sleep(self.wait)
+  # def vendor(self):
+  #   # Goes directly to the shop
+  #   self.go_to_room(1)
+  #   time.sleep(self.wait)
 
-  # Method to get treasure
-  # BFS Randomly to travel the maze, looting
-  # Once you get enough treasure, go sell
-  # Once you reach 1000 gold, buy a name
-    # Change name to something unique, that doesnt contain player
-  # Keep looting and selling until stopped.
-  def get_treasure(self):
-    while True:
-      if self.player.name.contains('player') and self.player.gold > 1000: # get a name
-        # Go to name changer (pirate ry)
-        print('Time to Buy a Name')
-        self.go_to_room(self.important_rooms['pirate ry'])
-        time.sleep(self.wait)
-        # Buy name
-        self.action('change_name')
-        time.sleep(self.wait)
-        # confirm_name
-        self.action('confirm_name')
-        print('Got a name! Time to get a COIN.', {self.player.name})
-        time.sleep(self.wait)
-        # self.action('status') #Check new name
-      elif player.encumbered <= player.strength - 2:
-        # If encumbered is str-2 (at base = 8)
-        # Travel the room bfs style at random
-        # Loot as you go with room_check
-        print('Looting..')
-        # self.explore_random(500)
-        self.go_to_room(str(random.choice(range(0,499))))
-        time.sleep(self.wait)
-      # Could potentially add a section to manage miner
-      else:
-        # else go directly to the shop
-        # loop through inventory and sell
-        # Go back to looting
-        print('Need to offload my loot.')
-        self.vendor()
-        print('At the shop, time to sell.')
-        for count in range(self.player.inventory):
-          print('Selling item..')
-          self.action('sell', item)
-          time.sleep(self.wait)
-          self.action('confirm_sell')
-          print({self.player.gold})
-          time.sleep(self.wait)
-        print('Back to Looting', {self.player.inventory})
+  # # Method to get treasure
+  # # BFS Randomly to travel the maze, looting
+  # # Once you get enough treasure, go sell
+  # # Once you reach 1000 gold, buy a name
+  #   # Change name to something unique, that doesnt contain player
+  # # Keep looting and selling until stopped.
+  # def get_treasure(self):
+  #   while True:
+  #     if self.player.name.contains('player') and self.player.gold > 1000: # get a name
+  #       # Go to name changer (pirate ry)
+  #       print('Time to Buy a Name')
+  #       self.go_to_room(self.important_rooms['pirate ry'])
+  #       time.sleep(self.wait)
+  #       # Buy name
+  #       self.action('change_name')
+  #       time.sleep(self.wait)
+  #       # confirm_name
+  #       self.action('confirm_name')
+  #       print('Got a name! Time to get a COIN.', {self.player.name})
+  #       time.sleep(self.wait)
+  #       # self.action('status') #Check new name
+  #     elif player.encumbered <= player.strength - 2:
+  #       # If encumbered is str-2 (at base = 8)
+  #       # Travel the room bfs style at random
+  #       # Loot as you go with room_check
+  #       print('Looting..')
+  #       # self.explore_random(500)
+  #       self.go_to_room(str(random.choice(range(0,499))))
+  #       time.sleep(self.wait)
+  #     # Could potentially add a section to manage miner
+  #     else:
+  #       # else go directly to the shop
+  #       # loop through inventory and sell
+  #       # Go back to looting
+  #       print('Need to offload my loot.')
+  #       self.vendor()
+  #       print('At the shop, time to sell.')
+  #       for count in range(self.player.inventory):
+  #         print('Selling item..')
+  #         self.action('sell', item)
+  #         time.sleep(self.wait)
+  #         self.action('confirm_sell')
+  #         print({self.player.gold})
+  #         time.sleep(self.wait)
+  #       print('Back to Looting', {self.player.inventory})
