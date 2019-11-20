934272719e2411d9116c41dd0841667897591de6
micahjones13 <micahjones13@gmail.com>
2019-11-20 14:25:14 -0700

    another one

--- explore.py
+++ explore.py
@@ -7,7 +7,7 @@ mappy.create_starting_map()
 
 # mappy.explore_random(150)
 # mappy.get_treasure()
-mappy.get_coins()
+# mappy.get_coins()
 # mappy.hint_to_ld8() #Translate the hint using ld8
 # mappy.go_to_room(476)
 # mappy.get_proof()
@@ -15,3 +15,4 @@ mappy.get_coins()
 #461 shrine
 # mappy.action('pray')
 # mappy.dash_to_room(476)
+mappy.auto_coins
